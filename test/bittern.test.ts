c0cadc621b396aec8c22d28b5f8a86c524e56d99
Greg Olmschenk <greg@olmschenk.com>
2026-08-23 23:31:28 -0400

    Replace remaining references to bittern with nena

--- test/bittern.test.ts
+++ test/bittern.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-infrastructure/core';
 // import { Template } from 'aws-cdk-infrastructure/assertions';
-// import * as Bittern from '../infrastructure/nena-stack';
+// import * as Nena from '../infrastructure/nena-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in infrastructure/lambda-function-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new Bittern.LambdaFunctionStack(app, 'MyTestStack');
+//   const stack = new Nena.LambdaFunctionStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
